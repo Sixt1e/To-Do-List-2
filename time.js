c33cd120d76cf04487119f327a709972fdd88034
Andrey <105484100+Sixt1e@users.noreply.github.com>
2022-12-19 11:29:38 +0300

    Add files via upload

---
+++ time.js
@@ -0,0 +1,22 @@
+function removeZeros(value)
+{
+    if (value < 10)
+    {value='0'+value;}
+    return value;
+} 
+
+function dateTime() {
+    let currentDatetime = new Date();
+    let day = removeZeros(currentDatetime.getDate());
+    let hours = removeZeros(currentDatetime.getHours());
+    let minutes = removeZeros(currentDatetime.getMinutes());
+    let seconds = removeZeros(currentDatetime.getSeconds());
+    let month = removeZeros(currentDatetime.getMonth()+1);
+    return day+"."+month+"."+" "+hours+":"+minutes+":"+seconds;
+}
+
+document.getElementById('time').innerHTML = dateTime();
+
+setInterval(function () {
+    document.getElementById('time').innerHTML = dateTime();
+}, 1000);
